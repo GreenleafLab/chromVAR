f2813677ec2727fc381b3bdb609753ecdd0ca677
Alicia Schep <aschep@stanford.edu>
2016-01-15 15:56:55 -0800

    Added new method ofr getting background peaks.  Should be faster, but still based on mahalanobis distance.

--- R/backgroundPeaks.R
+++ R/backgroundPeaks.R
@@ -8,6 +8,9 @@ backgroundPeaks <- setClass("backgroundPeaks",
                                       background_peaks = 'matrix'
                             ))
 
+
+
+
 #' sampleBackgroundPeaks
 #' 
 #' gets sample counts for background sets for a given peak set
@@ -25,12 +28,23 @@ sampleBackgroundPeaks <- function(object, peak_set, counts_mat, niterations = 50
   sample_mat = sparseMatrix(j = as.vector(object@background_peaks[peak_set,1:niterations]), 
                             i = rep(1:niterations, each = length(peak_set)), 
                             x=1, 
-                            dims = c(niterations, length(counts_mat@peaks)))
+                            dims = c(niterations, counts_mat@npeak))
 
   sampled_counts =  as.matrix(t(sample_mat %*% counts_mat@counts))
 
   return(sampled_counts)
 }
+
+Rcpp::cppFunction('NumericVector mymahaC(NumericVector X1, NumericVector X2, NumericVector y, NumericMatrix s) {
+  return (pow(X1 - y[0],2) * s(0,0) + (X1 - y[0])*(X2 - y[1])*(s(1,0) + s(0,1))  + pow(X2 - y[1],2)*s(1,1));
+}')
+
+# mymaha <- function(X, y, s){
+#   x1y1 = X[,1] - y[1]
+#   x2y2 = X[,2] - y[2]
+#   out = (x1y1**2) * s[1,1] + x1y1*x2y2*(s[2,1] + s[1,2])  + (x2y2**2)*s[2,2]
+#   return(out)
+# }
 
 
 #' getBackgroundPeakSets
@@ -58,9 +72,11 @@ getBackgroundPeakSets <- function(counts_mat, niterations = 50, window = 1000, B
   }
   norm_mat <- cbind(log10(counts_mat@fragments_per_peak + 1 ), counts_mat@peaks$bias)
   reflected <- add_reflections(norm_mat, window = window)
-  cov_mat = cov(norm_mat)
+  inv_cov_mat <- solve(cov(norm_mat))
+  X1 <- reflected$data[,1]
+  X2 <- reflected$data[,2]
   sample_peaks <- function(index){
-    mdist <- mahalanobis(reflected$data, norm_mat[index,], cov = cov_mat)
+    mdist <- mymahaC(X1, X2, norm_mat[index,], inv_cov_mat)
     mdist[reflected$identify(index)] = Inf
     closest <- which(mdist <= quantile(mdist, window/nrow(norm_mat), type=1))
     s <- sample(closest, niterations, replace = for_set)
@@ -73,6 +89,39 @@ getBackgroundPeakSets <- function(counts_mat, niterations = 50, window = 1000, B
   else if (niterations == 1){
     sampled_peaks = matrix(do.call(c,BiocParallel::bplapply(1:nrow(norm_mat), sample_peaks, BPPARAM = BPPARAM)), nrow = nrow(norm_mat), ncol =1)
   }
+  background_peaks = backgroundPeaks(background_peaks = sampled_peaks, peaks = counts_mat@peaks)
+  return(background_peaks)
+}
+
+#' getBackgroundPeakSets2
+#' 
+#' Function to get a set of background peaks for each peak based on GC content and # of counts
+#' across all samples
+#' @param counts_mat a fragmentCounts object
+#' @param niterations number of background peaks to sample
+#' @param window window size around peak in which to sample a background peak
+#' @param for_set computing background peaks for a set of peaks? affect whether sampling is done with replacement
+#' @return backgroundPeaks object
+#' @details Background peaks are chosen by finding the window nearest neighbors to a peak in terms of 
+#' GC content and # of fragments across samples using the Mahalanobis distance.  From those nearest
+#' neighbors, niterations peaks are sampled from that background.
+#' @export
+getBackgroundPeakSets2 <- function(counts_mat, niterations = 50, window = 500, for_set = TRUE){
+  stopifnot(inherits(counts_mat,"fragmentCounts"))
+  #if bias not available...
+  if ("bias" %ni% colnames(S4Vectors::mcols(counts_mat@peaks))){
+    stop("Peaks must have metadata column named 'bias'. Compute using compute_bias.")
+  }
+  if (!for_set && niterations > window){
+    stop("If for_set is FALSE, then niterations must be less than window")
+  }
+  norm_mat <- cbind(log10(counts_mat@fragments_per_peak + 1 ), counts_mat@peaks$bias)
+  reflected <- add_reflections(norm_mat, window = window)
+  chol_cov_mat <- chol(cov(norm_mat))
+  tmp_vals <- t(forwardsolve(t(chol_cov_mat),t(reflected$data)))  
+  nns <- FNN::get.knn(tmp_vals, k = window)$nn.index
+  sampled_peaks <- t(apply(nns, 1, function(x) reflected$replace(sample(x, niterations, replace = for_set))))
+  rm(nns)
   background_peaks = backgroundPeaks(background_peaks = sampled_peaks, peaks = counts_mat@peaks)
   return(background_peaks)
 }
