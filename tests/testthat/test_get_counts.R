90f8ef796132b667a1db5b2530eefc4ac96505f0
Alicia Schep <aschep@stanford.edu>
2016-03-23 14:53:51 -0700

    updated tests for reading in peaks, counts

---
+++ tests/testthat/test_get_counts.R
@@ -0,0 +1,37 @@
+context('fragmentCounts')
+
+# Read in data needed for tests ------------------------------------------------
+
+test_rg <- system.file("extdata", "test_RG.bam", package = "chromVAR")
+test_bam1 <- system.file("extdata", "test_single1.bam", package = "chromVAR")
+test_bam2 <- system.file("extdata", "test_single2.bam", package = "chromVAR")
+test_bam3 <- system.file("extdata", "test_single3.bam", package = "chromVAR")
+peaks_file <- system.file("extdata", "test_bed.txt", package = "chromVAR")
+test_peaks <- get_peaks(peaks_file)
+
+
+# Test fragment counts with RG ___________--------------------------------------
+
+test_that("can count fragments using RG tags", {
+  counts <- get_counts(test_rg, test_peaks, by_rg = TRUE, paired = TRUE)
+  expect_is(counts, "Matrix")
+  expect_equal_to_reference(counts, "counts_rg.rds")
+})
+
+
+# Test fragment counts with multiple bam --------------------------------------
+
+test_that("can count fragments with multiple bams", {
+  counts <- get_counts(c(test_bam1, test_bam2, test_bam3), test_peaks, by_rg = FALSE, paired = TRUE)
+  expect_is(counts, "Matrix")
+  expect_equal_to_reference(counts, "counts_multiple_bam.rds")
+})
+
+
+
+
+
+
+
+
+
--- tests/testthat/test_peaks.R
+++ tests/testthat/test_peaks.R
@@ -5,20 +5,19 @@ context("peaks")
 peaks_file <- system.file("extdata", "test_bed.txt", package = "chromVAR")
 
 test_that("can read peaks file", {
-  peaks <- read_peaks(peaks_file)
+  peaks <- get_peaks(peaks_file)
   expect_is(peaks, "GenomicRanges")
   expect_equal_to_reference(peaks, "test_peaks.rds")
 })
 
 test_that("can read peaks file with one extra column", {
-  peaks <- read_peaks(peaks_file, extra_cols = 4, extra_names = "name")
+  peaks <- get_peaks(peaks_file, extra_cols = c("name" = 4))
   expect_is(peaks, "GenomicRanges")
   expect_equal(colnames(mcols(peaks)), "name") 
 })
 
 test_that("can read peaks file with several extra columns", {
-  peaks <- read_peaks(peaks_file, extra_cols = c(4,5), 
-                      extra_names = c("name","score"))
+  peaks <- get_peaks(peaks_file, extra_cols = c("name" = 4, "score" = 5))
   expect_is(peaks, "GenomicRanges")
   expect_equal(colnames(mcols(peaks)), c("name","score")) 
 })
