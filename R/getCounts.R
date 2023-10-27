4aaa4511de8bf70da819421250871b14dfb85254
esonder <emanuel.sonder@hest.ethz.ch>
2023-10-27 08:28:33 +0000

    initial commit modified getCounts

---
+++ R/getCounts.R
@@ -0,0 +1,21 @@
+
+.importFragments <- function(){}
+
+.resizeRanges <- function(){}
+
+.getGCCountent <- function(){} 
+
+.getInsertionCounts <- function(){}
+
+.getFLD <- function(){}
+
+.weightFragments <- function(){}
+
+getCounts <- function(data,
+                      ranges,
+                      paired=TRUE,
+                      resize=TRUE, 
+                      width=100){
+  
+
+}
